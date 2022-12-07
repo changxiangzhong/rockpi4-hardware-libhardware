ec2ae87947a14a632cc97464f44ab40bc8b582b2
Xiangzhong Chang <xiangzhong.chang@telia.no>
2022-12-07 16:02:18 +0100

    Add missing header

--- modules/hello/Android.mk
+++ modules/hello/Android.mk
@@ -1,11 +1,21 @@
 LOCAL_PATH := $(call my-dir)
+
 include $(CLEAR_VARS)
-LOCAL_C_INCLUDES := hardware/libhardware
+
+LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_PROPRIETARY_MODULE := true
+LOCAL_SHARED_LIBRARIES := liblog
+#############
+# I'm following $hw/modules/gralloc/Android.mk 
+#############
 LOCAL_MODULE_TAGS := optional
 LOCAL_PRELINK_MODULE := false
-LOCAL_MODULE_RELATIVE_PATH := hw
-LOCAL_SHARED_LIBRARIES := liblog
+# LOCAL_C_INCLUDES := hardware/libhardware
+
 LOCAL_SRC_FILES := hello.c
+LOCAL_HEADER_LIBRARIES := libhardware_headers
+
 LOCAL_MODULE := hello.default
-LOCAL_LDLIBS := -L$(SYSROOT)/usr/lib -llog
+# LOCAL_LDLIBS := -L$(SYSROOT)/usr/lib -llog
+
 include $(BUILD_SHARED_LIBRARY)
--- modules/hello/hello.c
+++ modules/hello/hello.c
@@ -10,6 +10,7 @@
 #include <malloc.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #define DEVICE_NAME "/dev/hello"
 #define MODULE_NAME "Hello"
